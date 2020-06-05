ae71ebb21255254b152f265141ac216d3cf86a12
Akram Khan <akrammiru@gmail.com>
2020-06-05 13:35:20 +0600

    modified MessageList.java

--- src/main/java/com/example/application/MessageList.java
+++ src/main/java/com/example/application/MessageList.java
@@ -9,27 +9,28 @@ public class MessageList extends Div {
 
     public MessageList() {
         setClassName(getClass().getSimpleName());
-
     }
 
     public void addMessage(String from, Avataaar avatar, String text, boolean isCurrentUser) {
-
         Span fromContainer = new Span(new Text(from));
-
         fromContainer.addClassName(getClass().getSimpleName() + "-name");
+
         Div textContainer = new Div(new Text(text));
         textContainer.addClassName(getClass().getSimpleName() + "-bubble");
+
         Div avatarContainer = new Div(avatar, fromContainer);
         avatarContainer.addClassName(getClass().getSimpleName() + "-avatar");
+
         Div line = new Div(avatarContainer, textContainer);
         line.addClassName(getClass().getSimpleName() + "-row");
         add(line);
+
         if (isCurrentUser) {
             line.addClassName(getClass().getSimpleName() + "-row-currentUser");
             textContainer.addClassName(getClass().getSimpleName() + "-bubble-currentUser");
         }
+
         line.getElement().callJsFunction("scrollIntoView");
     }
+
 }
-
-
