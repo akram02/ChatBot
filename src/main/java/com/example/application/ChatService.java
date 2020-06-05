95a365eedd510b819f9f0becaec69af8dc345b3a
Akram Khan <akrammiru@gmail.com>
2020-06-05 14:04:41 +0600

    added chat service

---
+++ src/main/java/com/example/application/ChatService.java
@@ -0,0 +1,23 @@
+package com.example.application;
+
+import org.goldrenard.jb.configuration.BotConfiguration;
+import org.goldrenard.jb.core.Bot;
+import org.goldrenard.jb.core.Chat;
+
+public class ChatService {
+
+    private final Chat chatSession;
+
+    public ChatService() {
+        BotConfiguration botConfiguration = BotConfiguration.builder()
+                .name("alice")
+                .path("src/main/resources")
+                .build();
+        Bot bot = new Bot(botConfiguration);
+        chatSession = new Chat(bot);
+    }
+
+    public String answer(String message) {
+        return chatSession.multisentenceRespond(message);
+    }
+}
